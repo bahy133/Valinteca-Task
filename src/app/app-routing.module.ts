0a4b7eaa6248a159f9bd4fe741bf557543569a04
bahy133 <bahy713@gmail.com>
2022-12-09 22:09:52 +0200

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './Components/layout/layout.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { SuccessComponent } from './Components/success/success.component';
+import { WelcomeScreenComponent } from './Components/welcome-screen/welcome-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: WelcomeScreenComponent },
+      { path: 'Register', component: RegisterComponent },
+      { path: 'Success', component: SuccessComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeScreenComponent } from './Components/welcome-screen/welcome-screen.component';
+import { LayoutComponent } from './Components/layout/layout.component';
+import { SuccessComponent } from './Components/success/success.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeScreenComponent
+    WelcomeScreenComponent,
+    LayoutComponent,
+    SuccessComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
